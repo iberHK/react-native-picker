fbfb124b527bc21c1a96e4b157fc9ab36c87d260
Kim <chenjunjin@chenjunjindeMacBook-Pro.local>
2018-03-06 11:06:30 +0800

    删除MainPage 无用代码

--- src/MainPage.js
+++ src/MainPage.js
@@ -47,31 +47,6 @@ export default class MainPage extends BaseComponent {
         </TouchableOpacity >
     }
 
-    initTimerPickSource() {
-        let unit = ['年', '月', '日'];
-        let years = [];
-        let months = [];
-        let days = [];
-        let hours = [];
-        let minutes = [];
-        for (let i = 0; i < new Date().getFullYear() + 1 - new Date('1917-01-01').getFullYear(); i++) {
-            years.push(i + new Date('1917-01-01').getFullYear() + unit[0]);
-        }
-        for (let i = 1; i < 13; i++) {
-            months.push(i + unit[1]);
-        }
-        for (let i = 0; i < 24; i++) {
-            hours.push((Array(2).join(0) + i).slice(-2) + unit[3]);
-        }
-        for (let i = 1; i < 32; i++) {
-            days.push(i + unit[2]);
-        }
-        for (let i = 1; i < 61; i++) {
-            minutes.push(i + unit[4]);
-        }
-        return [years, months, days];
-    }
-
     render() {
         return <View style={{
             width: this.mScreenWidth, height: this.mScreenHeight,
